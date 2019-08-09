e3b983d3d90e989d7477d700fdb1a282589f3944
ekoneko <arzusyume@gmail.com>
2019-08-09 17:05:56 +0800

    feat: support open images from directory

--- packages/main/src/plugins/File.ts
+++ packages/main/src/plugins/File.ts
@@ -1,9 +1,16 @@
 import * as path from 'path'
 import * as fs from 'fs'
+import { promisify } from 'util'
 import { app } from 'electron'
+import { uniq } from 'lodash'
+import * as readChunk from 'read-chunk'
+import * as fileType from 'file-type'
 import { AbstractPlugin } from '../AbstractPlugin'
 import { Logger } from '../helpers/Logger'
 import { ImagePlayerPlugin } from './ImagePlayer'
+
+const readdir = promisify(fs.readdir)
+const getStat = promisify(fs.stat)
 
 export class FilePlugin extends AbstractPlugin {
   private beforeReadyStack: string[] = []
@@ -15,30 +22,34 @@ export class FilePlugin extends AbstractPlugin {
     app.on('open-file', this.handleOpenFile)
   }
 
-  ready() {
+  async ready() {
     this.isReady = true
     if (this.beforeReadyStack.length) {
       this.logger.info('open files before ready', this.beforeReadyStack)
+      this.beforeReadyStack = uniq(this.beforeReadyStack)
       this.openFile(this.beforeReadyStack)
     }
     const fileFromArgs = this.getFileFromArgs(process.argv)
     if (fileFromArgs) {
       this.logger.info('open files from args', fileFromArgs)
-      this.openFile([fileFromArgs])
+      const images = await this.extractImages(fileFromArgs)
+      this.openFile(images)
     }
   }
 
-  private handleOpenFile = (event: Electron.Event, filePath: string) => {
+  private handleOpenFile = async (event: Electron.Event, filePath: string) => {
+    const images = await this.extractImages(filePath)
     if (!this.isReady) {
-      this.beforeReadyStack.push(filePath)
+      Object.assign(this.beforeReadyStack, images)
     } else {
-      this.logger.info('open files', filePath)
-      this.openFile([filePath])
+      this.logger.info('on open files', filePath)
+      this.openFile(images)
     }
   }
 
-  private openFile = (filePaths: string[]) => {
-    this.getDepend<ImagePlayerPlugin>('imagePlayer').openFile(filePaths)
+  private openFile = async (images: string[]) => {
+    this.logger.info('open images', images)
+    this.getDepend<ImagePlayerPlugin>('imagePlayer').openFile(images)
   }
 
   private getFileFromArgs(argv: string[]): string | void {
@@ -48,5 +59,35 @@ export class FilePlugin extends AbstractPlugin {
         return arg
       }
     }
+  }
+
+  private async extractImages(filePath: string) {
+    const stat = await getStat(filePath)
+    if (stat.isDirectory()) {
+      return this.extractImagesFromDirectory(filePath)
+    }
+    if (!stat.isFile() || !(await this.isImage(filePath, false))) {
+      return [] as string[]
+    }
+
+    return [filePath]
+  }
+
+  private async extractImagesFromDirectory(dirPath: string) {
+    const files = await readdir(dirPath)
+    const filePaths = files.map((name) => path.join(dirPath, name))
+    const images = filePaths
+      .filter((file) => fs.statSync(file).isFile())
+      .filter((file) => this.isImage(file))
+    return images
+  }
+
+  private isImage(filePath: string, ignoreHideFile = true) {
+    if (path.basename(filePath)[0] === '.' && ignoreHideFile) {
+      return false
+    }
+    const buffer = readChunk.sync(filePath, 0, fileType.minimumBytes)
+    const type = fileType(buffer)
+    return type && type.mime.startsWith('image/')
   }
 }
